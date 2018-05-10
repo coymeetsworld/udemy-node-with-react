fba3fc0187c9f3ba96aa9177a0d8fa65bcf9ce31
Coy Sanders <coymeetsworld@gmail.com>
2018-05-10 02:14:01 -0700

    Added first route

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,6 +2,10 @@
 
 import React from 'react';
 
+// BrowserRouter - the brains, tells react-router how to behave. Looks at URL and says what to display
+// Route - to set a rule between URL and component
+import { BrowserRouter, Route} from 'react-router-dom';
+
 //dummy component (for now)
 const Header = () => <h2>Header</h2>
 const Dashboard = () => <h2>Dashboard</h2>
@@ -9,14 +13,15 @@ const SurveyNew = () => <h2>SurveyNew</h2>
 const Landing = () => <h2>Landing</h2>
 
 
-// BrowserRouter - the brains, tells react-router how to behave. Looks at URL and says what to display
-// Route - to set a rule between URL and component
-import { BrowserRouter, Route} from 'react-router-dom';
-
+// BrowserRouter expects AT MOST one child
 const App = () => {
   return (
     <div>
-      Hi There!
+      <BrowserRouter>
+        <div>
+          <Route path="/" component={Landing}/>
+        </div>
+      </BrowserRouter>
     </div>
   );
 };

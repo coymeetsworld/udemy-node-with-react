bc1bfd03cba4f99e0ddcc769a4288faf12a351f4
Coy Sanders <coymeetsworld@gmail.com>
2018-05-10 01:00:25 -0700

    Adding initial redux portion

--- client/src/index.js
+++ client/src/index.js
@@ -5,5 +5,23 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App';
 
+// Redux notes
+// Provider is a component that makes the store accessible to every component in the app.
+// Components like SurveyList can access the store thanks to the Provider tag
+// Provider is the bonding glue between React and Redux
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+// first argument dummy function
+// second argument initial state. We don't care so we make it empty object.
+// third argument for Redux-thunk (later)
+const store = createStore(() => [], {}, applyMiddleware());
+
 // 2nd argument is a reference to an existing DOM node in our HTML (public/index.html).
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  document.querySelector('#root')
+);
+

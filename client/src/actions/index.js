45f1db2b49614baf48388ad1bf1e61f482b724f0
Coy Sanders <coymeetsworld@gmail.com>
2018-05-10 14:13:04 -0700

    refactor fetchUser with async/await

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,26 +2,25 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-// forgot export
-export const fetchUser = () => {
+export const fetchUser = () => async dispatch => {
+
+  // Refactoring: Don't need return statement or curly brackets if there is only one return statement
 
   //applyMiddleware will see that we are returning a function from the action creator.
   // redux-thunk will automatically call the function, and pass the dispatch function as an argument.
   //
   // Were doing this so that we don't dispatch an action until the API request is completed, thats the whole point of this.
-  return function(dispatch) {
 
-    // path comes from authRoutes.js
-    axios.get('/api/current_user')
-         .then(res => dispatch({ type: FETCH_USER, payload: res}));
-    /*
-    Under package.json, setup this proxy for ALL api calls.
-      "/api/*": {
-      "target": "http://localhost:5000"
-    }*/
-  };
+  const res = await axios.get('/api/current_user')
+  dispatch({ type: FETCH_USER, payload: res});
   
+  /*
+  Under package.json, setup this proxy for ALL api calls.
+    "/api/*": {
+    "target": "http://localhost:5000"
+  }*/
 };
+  
 
 // redux thunk notes:
 // React component calls an action creator and produces an action.

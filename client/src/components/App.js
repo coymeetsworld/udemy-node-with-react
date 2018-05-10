fc39c37220bec5dcf682fb8e364b90ff9660b5e8
Coy Sanders <coymeetsworld@gmail.com>
2018-05-10 02:09:41 -0700

    Initial add of react-router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,6 +2,17 @@
 
 import React from 'react';
 
+//dummy component (for now)
+const Header = () => <h2>Header</h2>
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
+const Landing = () => <h2>Landing</h2>
+
+
+// BrowserRouter - the brains, tells react-router how to behave. Looks at URL and says what to display
+// Route - to set a rule between URL and component
+import { BrowserRouter, Route} from 'react-router-dom';
+
 const App = () => {
   return (
     <div>

9df751f6b2d2dece2f05fd30dfae92327694112e
Coy Sanders <coymeetsworld@gmail.com>
2018-05-16 00:11:49 -0700

    Adding a relationship field in Survey (User)

--- models/Survey.js
+++ models/Survey.js
@@ -8,7 +8,14 @@ const surveySchema = new Schema({
   subject: String,
   recipients: [RecipientSchema],
   yes: { type: Number, default: 0 },
-  no: { type: Number, default: 0 }
+  no: { type: Number, default: 0 },
+  _user: { type: Schema.Types.ObjectId, ref: 'User' },
+  dateSent: Date,
+  lastResponded: Date
 });
+// Every survey belongs to a specific User (referenced by the googleID of the User)
+// _ is not required, just a convention were using to say were using a relationship
+
+
 
 mongoose.model('surveys', surveySchema);

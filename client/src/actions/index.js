8d81ba9e4a388f9a9244b93e09f30fca2d91a0c6
Coy Sanders <coymeetsworld@gmail.com>
2018-05-19 20:57:32 -0700

    Posting to surveys

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -28,8 +28,9 @@ export const handleToken = token => async dispatch => {
 };
 
 
-export const submitSurvey = values => {
-  return { type: 'submit_survey'};
+export const submitSurvey = values => async dispatch => {
+  const res = await axios.post('/api/surveys', values);
+  dispatch({ type: FETCH_USER, payload: res.data });
 };
 
 // redux thunk notes:

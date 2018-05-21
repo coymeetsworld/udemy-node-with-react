0c80b95717840dd1b67d566d60ea5bd42fce806c
Coy Sanders <coymeetsworld@gmail.com>
2018-05-21 01:51:50 -0700

    Refactor with lodash chaining methods

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -17,18 +17,19 @@ module.exports = app => {
   });
 
   app.post('/api/surveys/webhooks', (req, res) => {
-    const events = _.map(req.body, ({ email, url }) => {
-      const pathname = new URL(url).pathname; //will just extract the route
-      const p = new Path('/api/surveys/:surveyId/:choice'); 
-      const match = p.test(pathname);
-      if (match) {
-        return { email, surveyId: match.surveyId, choice: match.choice};
-      }
-    });
+
+    const p = new Path('/api/surveys/:surveyId/:choice'); 
+
+    const events = _.chain(req.body) 
+      .map(({ email, url }) => {
+        const match = p.test(new URL(url).pathname);
+        if (match) return { email, surveyId: match.surveyId, choice: match.choice};
+      }) 
+      .compact() //removes undefined records
+      .uniqBy('email', 'surveyId')
+      .value();
     
-    const compactEvents = _.compact(events); //removes undefined records
-    const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
-    console.log(uniqueEvents);
+    console.log(events);
 
     res.send({});
   });
